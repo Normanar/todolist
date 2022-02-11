fd3e88be2ef055af8cf8d3944232522acc041e24
Arman Alimov <527c@mail.ru>
2022-02-11 20:53:50 +0600

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './App.css'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import {useSelector} from 'react-redux'
+import {AppRootStateType} from './store'
+import {RequestStatusType} from './app-reducer'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "../features/Login/Login";
+import {Routes, Route} from "react-router-dom";
+import {NotFound} from "../features/NotFound/NotFound";
+
+type PropsType = {
+    demo?: boolean
+}
+
+function App({demo = false}: PropsType) {
+    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    return (
+        <div className="App">
+            <ErrorSnackbar/>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+                {status === 'loading' && <LinearProgress/>}
+            </AppBar>
+            <Container fixed>
+                <Routes>
+                    {/*<TodolistsList demo={demo}/>*/}
+                    {/*<Login/>*/}
+                    <Route path="/" element={<TodolistsList demo={demo}/>}/>
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Container>
+        </div>
+    )
+}
+
+export default App
---
+++ src/features/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./NotFound.module.css"
+
+export const NotFound = () => {
+
+    return (
+        <div className={style.text}>
+            Ooops! Not found!
+        </div>
+    )
+}
