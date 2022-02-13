400c754f7c514a2fcf1971cf80126e53f0345f3f
Arman Alimov <527c@mail.ru>
2022-02-13 13:08:47 +0600

    formik

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -41,6 +41,7 @@ export const Login = () => {
         },
         onSubmit: values => {
             alert(JSON.stringify(values));
+            formik.resetForm();
         },
     })
 
@@ -62,24 +63,20 @@ export const Login = () => {
                     <FormGroup>
                         <TextField label="Email"
                                    margin="normal"
-                                   name="email"
-                                   onChange={formik.handleChange}
-                                   value={formik.values.email}
+                                   {...formik.getFieldProps('email')}
                         />
-                        {formik.errors.email && <div style={{color: "red"}}>{formik.errors.email}</div>}
+                        {formik.touched.email && formik.errors.email &&
+                        <div style={{color: "red"}}>{formik.errors.email}</div>}
                         <TextField type="password"
                                    label="Password"
                                    margin="normal"
-                                   name="password"
-                                   onChange={formik.handleChange}
-                                   value={formik.values.password}
+                                   {...formik.getFieldProps('password')}
                         />
-                        {formik.errors.password && <div style={{color: "red"}}>{formik.errors.password}</div>}
-                        <FormControlLabel name="rememberMe"
-                                          label={'Remember me'}
+                        {formik.touched.password && formik.errors.password &&
+                        <div style={{color: "red"}}>{formik.errors.password}</div>}
+                        <FormControlLabel label={'Remember me'}
                                           control={<Checkbox/>}
-                                          onChange={formik.handleChange}
-                                          checked={formik.values.rememberMe}
+                                          {...formik.getFieldProps('rememberMe')}
                         />
                         <Button type={'submit'} variant={'contained'} color={'primary'}>
                             Login
